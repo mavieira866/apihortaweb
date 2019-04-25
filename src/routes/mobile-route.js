532ed6e73d6c220b8eb39104015ff0a2c0824761
Vinicios Wentz <wentz.vinicios@gmail.com>
2019-04-24 21:49:27 -0300

    Prototyping mobile routes

--- src/routes/mobile-route.js
+++ src/routes/mobile-route.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const route = router.get("/", (req, res, next) => {
+router.get("/test", (req, res, next) => {
   res.status(200).send({
     Ambiente: process.env.NODE_ENV,
     Working: true
   });
 });
+
+router.get("/sensor-data/:id", (req, res, next) => {
+  res.status(200).send({
+    
+  })
+})
 
 module.exports = router;
